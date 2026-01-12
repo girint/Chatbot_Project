1776acc8aac0077907cc7bfcd387a0dfa589a356
Lsangsu <sangsu218@naver.com>
2026-01-12 12:11:00 +0900

    Revert "0107_1752_kty"

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,6 +1,6 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState, setVisible } from "react";
+import { useEffect, useState } from "react";
 import { AuthUtils } from "../../api/User_Api";
 
 const Header = () => {
@@ -33,11 +33,9 @@ const Header = () => {
 
   return (
     <>
-      <div style={{width:'100%', height:'115px'}}></div>
+      <div className="membership-mini-box">dd</div>
         <Navbar expand="lg" fixed="top" className="header-all-div" style={{ padding: "0" }}>
-          <div className="membership-mini-box">
-            <span>멤버쉽 가입으로 각종 혜택과 쿠폰을 가져가세요!</span>
-          </div>
+
           <Container fluid className="header-container">
             
             <Navbar.Brand href="/">
